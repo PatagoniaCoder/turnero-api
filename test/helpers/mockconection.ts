0973b45c4cd5d74e10f0340caf3c4aad5ca1662f
Alejandro Menendez <menendezanm@gmail.com>
2021-12-19 13:01:32 -0300

    Add sqlitre db to ignore

--- test/helpers/mockconection.ts
+++ test/helpers/mockconection.ts
@@ -4,13 +4,13 @@ import { join } from "path";
 const mockConnection: TypeOrmModuleOptions = {
     type:'sqlite',
     driver:require('sqlite3'),
-    database: "test_turnerodb",
+    database: "test_turnerodb.sql",
     entities: [join(__dirname, "../src/**/*/*entity.ts")], //['dist/**/*.entity{.ts,.js}'],
     autoLoadEntities: true,
     synchronize: true,
     logging: true,
     logger: "file",
-    dropSchema: false,
+    dropSchema: true,
     keepConnectionAlive: true,
   };
   export default mockConnection;
